98165d71580a6e8287b586ed3e35aed21d97462b
Hugo Prenat <hugo.prenat@epitech.eu>
2015-03-01 15:44:27 +0100

    Update algo

--- src/philo.c
+++ src/philo.c
@@ -5,23 +5,23 @@
 ** Login   <jibb@epitech.net>
 **
 ** Started on  Wed Feb 25 10:18:39 2015 Jean-Baptiste Grégoire
-** Last update Sun Mar  1 13:58:46 2015 Hugo Prenat
+** Last update Sun Mar  1 15:40:01 2015 Hugo Prenat
 */
 
 #include "window.h"
-
-pthread_mutex_t	disp = PTHREAD_MUTEX_INITIALIZER;
 
 void	change_to_eat(t_philo *philo)
 {
   pthread_mutex_lock(&philo->stick);
   if (pthread_mutex_trylock(&philo->right_philo->stick))
     {
+      //      printf("JE N'ACCEPTE PAS\n\n\n");
       pthread_mutex_unlock(&philo->stick);
       return ;
     }
   philo->state = EAT;
   philo->rice -= 1;
+  printf("ID: %d état : eat\n", philo->id);
   sleep(CYCLE_EAT);
   pthread_mutex_unlock(&philo->stick);
   pthread_mutex_unlock(&philo->right_philo->stick);
@@ -29,29 +29,20 @@ void	change_to_eat(t_philo *philo)
 
 void	change_to_sleep(t_philo *philo)
 {
+  if (philo->state != EAT)
+    return ;
   philo->state = SLEEP;
+  printf("ID: %d état : sleep\n", philo->id);
 }
 
 void	change_to_think(t_philo *philo)
 {
   pthread_mutex_lock(&philo->stick);
   philo->state = THINK;
+  printf("ID: %d état : think\n", philo->id);
   sleep(CYCLE_THINK);
   pthread_mutex_unlock(&philo->stick);
 }
-
-void			display(t_philo *philo)
-{
-  pthread_mutex_lock(&disp);
-  if (philo->state == SLEEP)
-    printf("ID: %d état : sleep\n", philo->id);
-  else if (philo->state == EAT)
-    printf("ID: %d état : eat\n", philo->id);
-  else
-    printf("ID: %d état : think\n", philo->id);
-  pthread_mutex_unlock(&disp);
-}
-
 
 void			*start_philo(void *philos)
 {
@@ -60,13 +51,12 @@ void			*start_philo(void *philos)
   philo = (t_philo *)(philos);
   while (philo->rice > 0 && philo->is_good)
     {
-      if (philo->state == EAT)
-	change_to_sleep(philo);
+      if (philo->state == THINK || philo->state == SLEEP)
+	change_to_eat(philo);
       else if (philo->state == SLEEP)
 	change_to_think(philo);
       else
-	change_to_eat(philo);
-      display(philo);
+	change_to_sleep(philo);
     }
   return ((void *)(0));
 }
